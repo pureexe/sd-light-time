9b9617cf78cb43333478d3cc90e927311bd883cc
Pakkapon Phongthawee <pure.gif@gmail.com>
2024-06-24 21:48:17 +0700

    save

---
+++ src/20240624/constants.py
@@ -0,0 +1,8 @@
+import os 
+FOLDER_NAME = "20240624"
+OUTPUT = f"output/{FOLDER_NAME}/"
+SRC = f"src/{FOLDER_NAME}/"
+DATASET_ROOT_DIR = "datasets/face/face2000_single/"
+OUTPUT_MULTI = f"output/{FOLDER_NAME}/multi_mlp_fit"
+OUTPUT_MULTI_MANUAL = f"output/{FOLDER_NAME}/multi_fit_manual"
+OUTPUT_SINGLE = f"output/{FOLDER_NAME}/single_fit/"
