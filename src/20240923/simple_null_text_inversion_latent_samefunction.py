b5414982104a4adfbf7d76e2971e612ba6295643
Pakkapon Phongthawee <pure.gif@gmail.com>
2024-09-28 21:48:54 +0700

    save

--- src/20240923/simple_null_text_inversion_latent_samefunction.py
+++ src/20240923/simple_null_text_inversion_latent_samefunction.py
@@ -13,10 +13,10 @@ SEED = 42
 DEVICE = "cuda"
 #PROMPT = "A dog in the park"
 PROMPT = "several pots of plants sit on a counter top"
-NUM_INFERENCE_STEPS = 3
+NUM_INFERENCE_STEPS = 50
 GUIDANCE_SCALE = 7.0
 NULL_STEP = 10
-EXP_NAME = f"latentinput_{NUM_INFERENCE_STEPS}_4"
+EXP_NAME = f"latentinput_samefunction_{NUM_INFERENCE_STEPS}_3"
 #INPUT_IMAGE = "src/20240923/copyroom10.png"
 INPUT_IMAGE = "copyroom10.png"
 
@@ -95,7 +95,6 @@ def main():
     }
     zt_noise, _ = pipe(**ddim_args)
     copy_zt_noise = zt_noise.clone()
-    #torch.save(zt_noise, f"zt_noise_{EXP_NAME}.pt")
 
     pipe.scheduler = nomral_scheduler
 
@@ -107,14 +106,11 @@ def main():
 
     null_embeddings = []
     null_timesteps = []
-
     null_latents = []
+    null_predicts = []
+    null_text_phase = "optimization"
 
     def callback_nulltext(pipe, step_index, timestep, callback_kwargs):
-                
-        # we can't predict next latents for the last step
-        # if step_index+1 == NUM_INFERENCE_STEPS:
-        #     return callback_kwargs
         
         null_timesteps.append(timestep)
         
@@ -130,57 +126,93 @@ def main():
             callback_kwargs['latents'] = latents
             return callback_kwargs
         
+        # compute noise_pred_text
 
-        with torch.enable_grad():
-            if True:
+        noise_pred_text = pipe.unet(
+            sample=latents,
+            timestep=timestep,
+            encoder_hidden_states=text_embbeding,
+            return_dict=False,
+            timestep_cond=callback_kwargs['timestep_cond'],
+            cross_attention_kwargs=pipe.cross_attention_kwargs,
+            added_cond_kwargs=callback_kwargs['added_cond_kwargs']
+        )[0]
+
+        # backpropt to get negative_prompt_embeds
+        with torch.enable_grad():                
+            negative_prompt_embeds.requires_grad = True
+            optimizer = torch.optim.Adam([negative_prompt_embeds], lr=1e-2)
+            pred_last = None
+            for _ in range(NULL_STEP):
+                optimizer.zero_grad()
+                # prepare for input
+                unet_kwargs = {
+                    "sample": latents,
+                    "timestep": timestep,
+                    "encoder_hidden_states": negative_prompt_embeds,
+                    "return_dict": False,
+                    "timestep_cond": callback_kwargs['timestep_cond'],
+                    "cross_attention_kwargs": pipe.cross_attention_kwargs,
+                    "added_cond_kwargs": callback_kwargs['added_cond_kwargs']
+                }
+                # support for controlnet
+                if 'down_block_res_samples' in callback_kwargs:
+                    unet_kwargs['down_block_additional_residuals'] = callback_kwargs['down_block_res_samples']
+                    unet_kwargs['mid_block_additional_residual'] = callback_kwargs['mid_block_res_sample']
+
+                noise_pred_uncond = pipe.unet(**unet_kwargs)[0]
                 
-                negative_prompt_embeds.requires_grad = True
-                optimizer = torch.optim.Adam([negative_prompt_embeds], lr=1e-2)
-                for _ in range(NULL_STEP):
-                    optimizer.zero_grad()
-                    # prepare for input
-                    embedd = torch.cat([negative_prompt_embeds, text_embbeding], dim=0)
-                    unet_kwargs = {
-                        "sample": latent_model_input,
-                        "timestep": timestep,
-                        "encoder_hidden_states": embedd,
-                        "return_dict": False,
-                        "timestep_cond": callback_kwargs['timestep_cond'],
-                        "cross_attention_kwargs": pipe.cross_attention_kwargs,
-                        "added_cond_kwargs": callback_kwargs['added_cond_kwargs']
-                    }
-                    # support for controlnet
-                    if 'down_block_res_samples' in callback_kwargs:
-                        unet_kwargs['down_block_additional_residuals'] = callback_kwargs['down_block_res_samples']
-                        unet_kwargs['mid_block_additional_residual'] = callback_kwargs['mid_block_res_sample']
+                # classifier free guidance
+                #noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+                noise_pred = noise_pred_uncond + pipe.guidance_scale * (noise_pred_text - noise_pred_uncond)
 
-                    noise_pred = pipe.unet(**unet_kwargs)[0]
+                # Rescale noise cfg, Based on 3.4. in https://arxiv.org/pdf/2305.08891.pdf
+                noise_pred = rescale_noise_cfg(noise_pred, noise_pred_text, guidance_rescale=pipe.guidance_scale)
+                
+                # predict next latents
+                callback_kwargs['extra_step_kwargs']['generator'] = torch.Generator().manual_seed(SEED)
+                predict_latents = pipe.scheduler.step(noise_pred, timestep, latents,  **callback_kwargs['extra_step_kwargs'], return_dict=False)[0]
+                
+                # calculate loss with next latents
+                loss = torch.nn.functional.mse_loss(predict_latents, ddim_latents[step_index+1])
+                loss.backward()
+                optimizer.step()
+    
+                print(f"{loss.item():.6f}")
+                if loss < 1e-5: #early stopping mention in the paper
+                    break
 
-                    # classifier free guidance
-                    noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
-                    noise_pred = noise_pred_uncond + pipe.guidance_scale * (noise_pred_text - noise_pred_uncond)
+        # compute noise uncond for final time after all updateded
+        noise_pred_uncond = pipe.unet(
+            sample=latents,
+            timestep=timestep,
+            encoder_hidden_states=negative_prompt_embeds,
+            return_dict=False,
+            timestep_cond=callback_kwargs['timestep_cond'],
+            cross_attention_kwargs=pipe.cross_attention_kwargs,
+            added_cond_kwargs=callback_kwargs['added_cond_kwargs']
+        )[0]
 
-                    # Rescale noise cfg, Based on 3.4. in https://arxiv.org/pdf/2305.08891.pdf
-                    noise_pred = rescale_noise_cfg(noise_pred, noise_pred_text, guidance_rescale=pipe.guidance_scale)
-                    
-                    # predict next latents
-                    callback_kwargs['extra_step_kwargs']['generator'] = torch.Generator().manual_seed(SEED)
-                    predict_latents = pipe.scheduler.step(noise_pred, timestep, latents,  **callback_kwargs['extra_step_kwargs'], return_dict=False)[0]
-                    
-                    # calculate loss with next latents
-                    loss = torch.nn.functional.mse_loss(predict_latents, ddim_latents[step_index+1])
-                    loss.backward()
-                    optimizer.step()
-       
-                    print(f"{loss.item():.6f}")
-                    if loss < 1e-5: #early stopping mention in the paper
-                        break
+        noise_out = torch.cat([noise_pred_uncond, noise_pred_text], dim=0) # this is for later check
+
+         # classifier free guidance
+        #noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+        noise_pred = noise_pred_uncond + pipe.guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+        # Rescale noise cfg, Based on 3.4. in https://arxiv.org/pdf/2305.08891.pdf
+        noise_pred = rescale_noise_cfg(noise_pred, noise_pred_text, guidance_rescale=pipe.guidance_scale)
+                
+        # predict next latents
+        callback_kwargs['extra_step_kwargs']['generator'] = torch.Generator().manual_seed(SEED)
+        predict_latents = pipe.scheduler.step(noise_pred, timestep, latents,  **callback_kwargs['extra_step_kwargs'], return_dict=False)[0]
+        
 
         negative_prompt_embeds = negative_prompt_embeds.detach()
         callback_kwargs['prompt_embeds'] = torch.cat([negative_prompt_embeds, text_embbeding])
         null_embeddings.append(negative_prompt_embeds)
         callback_kwargs['latents'] = predict_latents.detach()
         null_latents.append(predict_latents)
+        null_predicts.append(noise_out)
         return callback_kwargs
     
     
@@ -199,9 +231,7 @@ def main():
     output_image.save(f"output_copyroom10_{EXP_NAME}.jpg")
 
 
-    #torch.save(null_embeddings, f"null_embeddings_{EXP_NAME}.pt")
-
-    def callback_apply_nulltext_OLD(pipe, step_index, timestep, callback_kwargs):
+    def callback_apply_nulltext(pipe, step_index, timestep, callback_kwargs):
         # skip if out of index 
         try:
             negative_text_embedding = null_embeddings[step_index+1]
@@ -215,72 +245,90 @@ def main():
         # apply null text
         callback_kwargs['prompt_embeds'] = torch.cat([negative_text_embedding, text_embbeding])
         return callback_kwargs
-    
-    def callback_apply_nulltext(pipe, step_index, timestep, callback_kwargs):
-        # we can't predict next latents for the last step
-        # if step_index+1 == NUM_INFERENCE_STEPS:
-        #     return callback_kwargs
 
-
+    def callback_apply_nulltext_WORK(pipe, step_index, timestep, callback_kwargs):
+                
         latent_model_input = callback_kwargs['latent_model_input'].clone().detach()
 
         latents = callback_kwargs['latent_model_input'].chunk(2)[0].clone().detach() # this trick only work for scheduler that don't scale input such as DDIM
 
         negative_prompt_embeds, text_embbeding = callback_kwargs['prompt_embeds'].chunk(2)
-        negative_prompt_embeds = negative_prompt_embeds.clone().detach()
+        #negative_prompt_embeds = negative_prompt_embeds.clone().detach()
+        negative_prompt_embeds = null_embeddings[step_index].clone().detach()
 
         # we can't predict next latents for the last step
         if step_index+1 == NUM_INFERENCE_STEPS:
             callback_kwargs['latents'] = latents
             return callback_kwargs
         
+        noise_pred_text = pipe.unet(
+            sample=latents,
+            timestep=timestep,
+            encoder_hidden_states=text_embbeding,
+            return_dict=False,
+            timestep_cond=callback_kwargs['timestep_cond'],
+            cross_attention_kwargs=pipe.cross_attention_kwargs,
+            added_cond_kwargs=callback_kwargs['added_cond_kwargs']
+        )[0]
 
-        with torch.enable_grad():
+        # check if noise_pred_text match
+        if torch.allclose(noise_pred_text, null_predicts[step_index].chunk(2)[1], atol=1e-5):
+            print("noise_pred_text: Match")
+        else:
+            print("noise_pred_text: Not Match")
+        
+
+        with torch.no_grad():                
             if True:
-                try:
-                    negative_prompt_embeds = null_embeddings[step_index].clone().detach()
-                except IndexError:
-                    latents = callback_kwargs['latent_model_input'].chunk(2)[0].clone().detach() # this trick only work for scheduler that don't scale input such as DDIM
-                    callback_kwargs['latents'] = latents
-                    return callback_kwargs
-
-                # why this is different with HF result. need check compare to stable diffusion pipeline
                 # prepare for input
-                embedd = torch.cat([negative_prompt_embeds, text_embbeding], dim=0)
+                #embedd = torch.cat([negative_prompt_embeds, text_embbeding], dim=0)
+                embedd = text_embbeding
                 unet_kwargs = {
-                    "sample": latent_model_input,
+                    "sample": latents, #latent_model_input,
                     "timestep": timestep,
-                    "encoder_hidden_states": embedd,
+                    "encoder_hidden_states": null_embeddings[step_index],
                     "return_dict": False,
                     "timestep_cond": callback_kwargs['timestep_cond'],
                     "cross_attention_kwargs": pipe.cross_attention_kwargs,
                     "added_cond_kwargs": callback_kwargs['added_cond_kwargs']
-
                 }
                 # support for controlnet
                 if 'down_block_res_samples' in callback_kwargs:
                     unet_kwargs['down_block_additional_residuals'] = callback_kwargs['down_block_res_samples']
                     unet_kwargs['mid_block_additional_residual'] = callback_kwargs['mid_block_res_sample']
-
-                noise_pred = pipe.unet(**unet_kwargs)[0]
                 
+                noise_pred_uncond = pipe.unet(**unet_kwargs)[0]
+                # check if later pass match 
+                #if torch.allclose(noise_pred.chunk(2)[1], null_predicts[step_index].chunk(2)[1], atol=1e-5):
+                if torch.allclose(noise_pred_uncond, null_predicts[step_index].chunk(2)[0], atol=1e-5):
+                    print("noise_pred_uncond: Match")
+                else:
+                    print("noise_pred_uncond: Not Match")
+
+                #noise_pred = null_predicts[step_index]
+
                 # classifier free guidance
-                noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+                #noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
                 noise_pred = noise_pred_uncond + pipe.guidance_scale * (noise_pred_text - noise_pred_uncond)
 
                 # Rescale noise cfg, Based on 3.4. in https://arxiv.org/pdf/2305.08891.pdf
                 noise_pred = rescale_noise_cfg(noise_pred, noise_pred_text, guidance_rescale=pipe.guidance_scale)
                 
+                
                 # predict next latents
-                predict_latents = pipe.scheduler.step(noise_pred, timestep, latents, **...['extra_step_kwargs'] , return_dict=False)[0]
-        
+                callback_kwargs['extra_step_kwargs']['generator'] = torch.Generator().manual_seed(SEED)
+                predict_latents = pipe.scheduler.step(noise_pred, timestep, latents,  **callback_kwargs['extra_step_kwargs'], return_dict=False)[0]
+                
+
         # compare different from null_latents
         loss = torch.nn.functional.mse_loss(predict_latents, null_latents[step_index])
         print(f"Loss: {loss.item():.6f}")
-    
+
         negative_prompt_embeds = negative_prompt_embeds.detach()
         callback_kwargs['prompt_embeds'] = torch.cat([negative_prompt_embeds, text_embbeding])
+        null_embeddings.append(negative_prompt_embeds)
         callback_kwargs['latents'] = predict_latents.detach()
+        null_latents.append(predict_latents)
         return callback_kwargs
     
     pipe_args = {
