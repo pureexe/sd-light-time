727b52cb1e88e283bca2036d7fffa1ae6abd8035
Pakkapon Phongthawee <pure.gif@gmail.com>
2024-10-12 10:44:55 +0000

    update permission

--- src/20241008/generate_ddim_1scene.sh
+++ src/20241008/generate_ddim_1scene.sh
@@ -16,8 +16,8 @@ CUDA_VISIBLE_DEVICES=1 bin/py src/20241008/val_ddim_strength.py -m multillum_tes
 CUDA_VISIBLE_DEVICES=2 bin/py src/20241008/val_ddim_strength.py -m multillum_test1_4light_strength -g 1,1.25,1.5,1.75,2,2.25,2.5,2.75,3,4,5,6,7 -s  0.25,0.3,0.35,0.4 -i 36
 CUDA_VISIBLE_DEVICES=3 bin/py src/20241008/val_ddim_strength.py -m multillum_test1_4light_strength -g 1,1.25,1.5,1.75,2,2.25,2.5,2.75,3,4,5,6,7 -s  0.25,0.3,0.35,0.4 -i 37
 
-#v3
-CUDA_VISIBLE_DEVICES=0 bin/py src/20241008/val_ddim_strength.py -m multillum_test1_4light_strength -g 1,1.25,1.5,1.75,2,2.25,2.5,2.75,3,4,5,6,7 -s 0.45,0.5,0.55,0.6 -i 33
+#v14
+CUDA_VISIBLE_DEVICES=0 bin/si src/20241008/val_ddim_strength.py -m multillum_test1_4light_strength -g 1,1.25,1.5,1.75,2,2.25,2.5,2.75,3,4,5,6,7 -s 0.45,0.5,0.55,0.6 -i 33
 CUDA_VISIBLE_DEVICES=1 bin/py src/20241008/val_ddim_strength.py -m multillum_test1_4light_strength -g 1,1.25,1.5,1.75,2,2.25,2.5,2.75,3,4,5,6,7 -s 0.45,0.5,0.55,0.6 -i 35
 CUDA_VISIBLE_DEVICES=2 bin/py src/20241008/val_ddim_strength.py -m multillum_test1_4light_strength -g 1,1.25,1.5,1.75,2,2.25,2.5,2.75,3,4,5,6,7 -s 0.45,0.5,0.55,0.6 -i 36
 CUDA_VISIBLE_DEVICES=3 bin/py src/20241008/val_ddim_strength.py -m multillum_test1_4light_strength -g 1,1.25,1.5,1.75,2,2.25,2.5,2.75,3,4,5,6,7 -s 0.45,0.5,0.55,0.6 -i 37
