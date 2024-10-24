95b5f73f6cf192a96bbf45b8f52fc4883cbc9280
Pakkapon Phongthawee <pure.gif@gmail.com>
2024-10-24 11:12:04 +0700

    save

--- src/20241021/val_ddim.py
+++ src/20241021/val_ddim.py
@@ -17,7 +17,7 @@ import argparse
 from constants import FOLDER_NAME
 
 MASTER_TYPE = 16
-CHECKPOINT_FOLDER_NAME = "20241012"
+CHECKPOINT_FOLDER_NAME = "20241021"
 
 
 parser = argparse.ArgumentParser()
@@ -29,11 +29,9 @@ parser.add_argument("-c", "--checkpoint", type=str, default="59")
 args = parser.parse_args()
 NAMES = {
     0: 'depth',
-    1: 'both_bae'
+    1: 'both_bae',
     2: 'bae',
     3: 'no_control',
-    4: 'bae',
-    5: 'no_control',
 }
 METHODS = {
     0: 'vae',
@@ -41,84 +39,39 @@ METHODS = {
     2: 'vae',
     3: 'vae',
     4: 'vae',
-    5: 'vae',
 }
 CONDITIONS_CLASS = {
     0: AffineDepth,
     1: AffineDepthNormalBae,
     2: AffineNormalBae,
     3: AffineNoControl,
-    4: AffineNormalBae,
-    5: AffineNoControl,
 }
 LRS = {
     0: '1e-4',
     1: '1e-4',
     2: '1e-4',
     3: '1e-4',
-    4: '1e-4',
-    5: '1e-4',
  }
 DIRNAME = {
+    0: CHECKPOINT_FOLDER_NAME,
+    1: CHECKPOINT_FOLDER_NAME,
     2: CHECKPOINT_FOLDER_NAME,
     3: CHECKPOINT_FOLDER_NAME,
-    4: CHECKPOINT_FOLDER_NAME,
-    5: CHECKPOINT_FOLDER_NAME,
 }
 
 
 def get_from_mode(mode):
-    if mode == "ddim_left2right":
-        return "/data/pakkapon/datasets/unsplash-lite/train_under", 1, DDIMUnsplashLiteDataset,{'index_file': 'src/20240824/ddim_left2right100.json'}, None
-    elif mode == "ddim_left2right_dev":
-        return "/data/pakkapon/datasets/unsplash-lite/train_under", 1, DDIMUnsplashLiteDataset,{'index_file': 'src/20240824/ddim_dev.json'}, None
-    elif mode == "face10":
-        return "datasets/face10", 1000, DDIMCrossDataset,{'index_file': None, 'envmap_file':'datasets/face10/target_envmap.json', 'envmap_dir':"/data/pakkapon/datasets/unsplash-lite/train_under" }, None
-    elif mode == "shoe":
-        return "/data/pakkapon/datasets/shoe_validation", 60, DDIMDataset, {'index_file': '/data/pakkapon/datasets/shoe_validation/ddim.json'}, None
-    elif mode == "shoe_trainlight2":
-        control_paths = {
-            'control_depth': '/data/pakkapon/datasets/shoe_validation/control_depth/00000.png',
-            'control_normal': '/data/pakkapon/datasets/shoe_validation/control_normal/00000.png', 
-            'control_normal_bae': '/data/pakkapon/datasets/shoe_validation/control_normal_bae/00000.png',
-        }
-        source_env_ldr = '/data/pakkapon/datasets/shoe_validation/env_ldr/00000.png'
-        source_env_under = '/data/pakkapon/datasets/shoe_validation/env_under/00000.png'
-        image_path = '/data/pakkapon/datasets/shoe_validation/images/00000.png'
-        return "/data/pakkapon/datasets/unsplash-lite/train_under", 10, DDIMSingleImageDataset, {'index_file': 'src/20240902/ddim_10right1left.json', 'image_path': image_path, 'control_paths': control_paths, 'source_env_ldr': source_env_ldr, 'source_env_under': source_env_under}, None
-    elif mode == "face_left_ddim_v2":
-        return "/data/pakkapon/datasets/face/face2000", 100, DDIMDataset,{"index_file":"/data/pakkapon/datasets/face/face2000/split-x-minus.json"}, None
-    elif mode == "face_right_ddim_v2":
-        return "/data/pakkapon/datasets/face/face2000", 100, DDIMDataset,{"index_file":"/data/pakkapon/datasets/face/face2000/split-x-plus.json"}, None
-    elif mode == "face_identity_guidacne_hfcode":
-        return "/data/pakkapon/datasets/face/face2000", 100, DDIMDataset,{"index_file":"/data/pakkapon/datasets/face/face2000/split-identity.json"}, None
-    elif mode == "face_identity_guidacne_hfcode":
-        return "/data/pakkapon/datasets/face/face2000", 100, DDIMDataset,{"index_file":"/data/pakkapon/datasets/face/face2000/split-identity.json"}, None
-    elif mode == "multillum_train_v2":
-        return "/data/pakkapon/datasets/multi_illumination/spherical/train", 100, DDIMSHCoeffsDataset,{"index_file":"/data/pakkapon/datasets/multi_illumination/spherical/split-train3scenes.json"}, None
-    elif mode == "multillum_test_v2":
-        return "/data/pakkapon/datasets/multi_illumination/spherical/test", 100, DDIMSHCoeffsDataset,{"index_file":"/data/pakkapon/datasets/multi_illumination/spherical/split-test3scenes.json"}, None
-    elif mode == "multillum_val_array_v3":
-        return "/data/pakkapon/datasets/multi_illumination/spherical/val", 100, DDIMArrayEnvDataset,{"index_file":"/data/pakkapon/datasets/multi_illumination/spherical/split-val-relight-array.json"}, None   
-    elif mode == "multillum_val":
-        return "/data/pakkapon/datasets/multi_illumination/spherical/val", 100, DDIMDataset,{"index_file":"/data/pakkapon/datasets/multi_illumination/spherical/split-val-relight.json"}, None   
-    elif mode == "multillum_val_rotate_test":
-        return "/data/pakkapon/datasets/multi_illumination/spherical/val_rotate", 100, DDIMArrayEnvDataset,{"index_file":"/data/pakkapon/datasets/multi_illumination/spherical/val_rotate/split.json"}, None   
-    elif mode == "multillum_test_30_array_v2":
-        return "/data/pakkapon/datasets/multi_illumination/spherical/test", 100, DDIMArrayEnvDataset,{"index_file":"/data/pakkapon/datasets/multi_illumination/spherical/split-test-30-array.json"}, None
-    elif mode == "multillum_train2_nulltext":
+    if mode == "multillum_train":
         return "/data/pakkapon/datasets/multi_illumination/spherical/train", 100, DDIMArrayEnvDataset,{"index_file":"/data/pakkapon/datasets/multi_illumination/spherical/split-train2-relight-array.json"}, None
-    elif mode == "multillum_train2_nulltext":
-        return "/data/pakkapon/datasets/multi_illumination/spherical/train", 100, DDIMArrayEnvDataset,{"index_file":"/data/pakkapon/datasets/multi_illumination/spherical/split-train2-relight-array.json"}, None
-    elif mode == "multillum_train2":
-        return "/data/pakkapon/datasets/multi_illumination/spherical/train", 100, DDIMArrayEnvDataset,{"index_file":"/data/pakkapon/datasets/multi_illumination/spherical/split-train2-relight-array.json"}, "a photo realistic image"
-    elif mode == "multillum_test3":
-        return "/data/pakkapon/datasets/multi_illumination/spherical/test", 100, DDIMArrayEnvDataset,{"index_file":"/data/pakkapon/datasets/multi_illumination/spherical/split-test3-relight-array.json"}, "a photo realistic image"
+    elif mode == "multillum_test":
+        return "/data/pakkapon/datasets/multi_illumination/spherical/test", 100, DDIMArrayEnvDataset,{"index_file":"/data/pakkapon/datasets/multi_illumination/spherical/split-test3-relight-array.json"}, None
+    elif mode == "multillum_test2":
+        return "/data/pakkapon/datasets/multi_illumination/spherical/test", 100, DDIMArrayEnvDataset,{"index_file":"/data/pakkapon/datasets/multi_illumination/spherical/split-test3-relight24-array.json"}, None
     else:
         raise Exception("mode not found")
 
 def main():
-    CONDITIONS_CLASS[0] = AffineNoControl
+    print("STARTING VALIDATION...")
     versions = [int(a.strip()) for a in args.version.split(",")]
     guidance_scales = [float(a.strip()) for a in args.guidance_scale.split(",")]
     checkpoints = [int(a.strip()) for a in args.checkpoint.split(",")]
@@ -126,11 +79,8 @@ def main():
 
     for mode in modes:
         for version in versions:
-                #condition_class = CONDITIONS_CLASS[version]
-                #ddim_class = create_ddim_inversion(condition_class)
                 ddim_class = CONDITIONS_CLASS[version]
                 try:
-                #if True:
                     for checkpoint in checkpoints:
                         dirname = DIRNAME[version]
                         if checkpoint == 0:
@@ -169,8 +119,8 @@ def main():
                             trainer.test(model, dataloaders=val_dataloader, ckpt_path=CKPT_PATH)
 
 
-                except:
-                    pass
+                except Exception as e:
+                    raise e
 
                                 
 if __name__ == "__main__":
--- src/20241021/val_script.sh
+++ src/20241021/val_script.sh
@@ -1,5 +1,16 @@
-NODE=v23 GPU=0 NAME=val_depth bin/siat src/20241015/val_ddim_mix.py
+NODE=v16 GPU=0 NAME=val_depth bin/silocal src/20241021/val_ddim.py -m "multillum_train,multillum_test" -i 0 -c 79 -g 1,3,5,7
+NODE=v16 GPU=1 NAME=val_both_bae bin/silocal src/20241021/val_ddim.py -m "multillum_train,multillum_test" -i 1 -c 79 -g 1,3,5,7
+NODE=v16 GPU=2 NAME=val_bae bin/silocal src/20241021/val_ddim.py -m "multillum_train,multillum_test" -i 2 -c 79 -g 1,3,5,7
+NODE=v16 GPU=3 NAME=val_nocontrol bin/silocal src/20241021/val_ddim.py -m "multillum_train,multillum_test" -i 3 -c 79 -g 1,3,5,7
 
-NODE=v23 GPU=1 NAME=val_depth_checkpoint bin/siat src/20241015/val_ddim_mix.py -c 4,9,14,19,24,29,34,39,44,49,54,59,64
 
-NODE=v23 GPU=0 NAME=val_depth_checkpoint bin/siat src/20241015/val_ddim_mix.py -c 0
+# generate 
+
+NODE=v28 GPU=0 NAME=val0 bin/siat4080 src/20241021/val_ddim.py -m "multillum_test2" -i 0 -c 79 -g 1,3,5,7
+NODE=v28 GPU=1 NAME=val1 bin/siat4080 src/20241021/val_ddim.py -m "multillum_test2" -i 1 -c 79 -g 1,3,5,7
+NODE=v28 GPU=2 NAME=val2 bin/siat4080 src/20241021/val_ddim.py -m "multillum_test2" -i 2 -c 79 -g 1,3,5,7
+NODE=v27 GPU=0 NAME=val3 bin/siat4080 src/20241021/val_ddim.py -m "multillum_test2" -i 3 -c 79 -g 1,3,5,7
+NODE=v27 GPU=1 NAME=val4 bin/siat4080 src/20241021/val_ddim.py -m "multillum_test2" -i 0 -c 79 -g 5,7
+NODE=v27 GPU=2 NAME=val5 bin/siat4080 src/20241021/val_ddim.py -m "multillum_test2" -i 1 -c 79 -g 5,7
+NODE=v26 GPU=0 NAME=val6 bin/siat4080 src/20241021/val_ddim.py -m "multillum_test2" -i 2 -c 79 -g 5,7
+NODE=v26 GPU=1 NAME=val7 bin/siat4080 src/20241021/val_ddim.py -m "multillum_test2" -i 3 -c 79 -g 5,7
