b335cc5e63feebb4bcbdd18ecb99a43d05723d83
pakkapon phongthawee <pure.gif@gmail.com>
2024-10-26 00:52:18 +0700

    save

--- src/20241025/run_script.sh
+++ src/20241025/run_script.sh
@@ -5,3 +5,14 @@ NODE=v26 GPU=3 NAME=face1e-5 bin/siat4080 src/20241025/train.py -lr 1e-5 -ct no_
 NODE=v27 GPU=3 NAME=face5e-5 bin/siat4080 src/20241025/train.py -lr 5e-5 -ct no_control --feature_type shcoeff_order2 --guidance_scale 1.0 --batch_size 4
 
 NODE=v28 GPU=3 NAME=face5e-5 bin/siat4080 src/20241025/train.py -lr 5e-3 -ct no_control --feature_type shcoeff_order2 --guidance_scale 1.0 --batch_size 4
+
+
+# static primpt 
+cd /ist/ist-share/vision/pakkapon/relight/sd-light-time
+bin/siatv100 src/20241025/train.py -lr 1e-4 -ct no_control --feature_type shcoeff_order2 --guidance_scale 1.0 --batch_size 16 
+
+bin/siatv100 src/20241025/train.py -lr 5e-5 -ct no_control --feature_type shcoeff_order2 --guidance_scale 1.0 --batch_size 16 
+
+bin/siatv100 src/20241025/train.py -lr 1e-5 -ct no_control --feature_type shcoeff_order2 --guidance_scale 1.0 --batch_size 16 
+
+# parser.add_argument('-specific_prompt', type=str, default="a photorealistic image") 
