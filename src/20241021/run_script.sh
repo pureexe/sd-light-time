29aee13d9e6b5bb41a6e790bb2627626c209d03a
Pakkapon Phongthawee <pure.gif@gmail.com>
2024-10-28 19:00:40 +0700

    save

--- src/20241021/run_script.sh
+++ src/20241021/run_script.sh
@@ -14,6 +14,16 @@ NODE=v23 GPU=2 NAME=bae bin/silocal src/20241021/train.py -lr 1e-4 -ct bae --fea
 # v5 is resume from v3
 NODE=v23 GPU=3 NAME=no_control bin/silocal src/20241021/train.py -lr 1e-4 -ct no_control --feature_type vae --guidance_scale 3.0 --batch_size 12 -ckpt output/20241021/multi_mlp_fit/lightning_logs/version_3/checkpoints/epoch=000209.ckpt
 
-# v23 - both_bae
-# v23 - bae 
-# v23 - deepfloyd
+
+# v6 depth - continue from v0
+NODE=v9 GPU=0 NAME=depth bin/silocal src/20241021/train.py -lr 1e-4 -ct depth --feature_type vae --guidance_scale 3.0 --batch_size 12 -ckpt output/20241021/multi_mlp_fit/lightning_logs/version_0/checkpoints/epoch=000234.ckpt
+
+# v7 both_bae - continue from v1
+NODE=v9 GPU=1 NAME=both_bae bin/silocal src/20241021/train.py -lr 1e-4 -ct both_bae --feature_type vae --guidance_scale 3.0 --batch_size 12 -ckpt output/20241021/multi_mlp_fit/lightning_logs/version_1/checkpoints/epoch=000214.ckpt
+
+# v8  bae from v4
+NODE=v23 GPU=2 NAME=bae bin/silocal src/20241021/train.py -lr 1e-4 -ct bae --feature_type vae --guidance_scale 3.0 --batch_size 12 -ckpt output/20241021/multi_mlp_fit/lightning_logs/version_4/checkpoints/epoch=000234.ckpt
+
+# v8  no_control from v5
+NODE=v23 GPU=3 NAME=no_control bin/silocal src/20241021/train.py -lr 1e-4 -ct no_control --feature_type vae --guidance_scale 3.0 --batch_size 12 -ckpt output/20241021/multi_mlp_fit/lightning_logs/version_5/checkpoints/epoch=000259.ckpt
+
--- src/20241025/run_script.sh
+++ src/20241025/run_script.sh
@@ -4,4 +4,7 @@ NODE=v26 GPU=3 NAME=face1e-5 bin/siat4080 src/20241025/train.py -lr 1e-5 -ct no_
 
 NODE=v27 GPU=3 NAME=face5e-5 bin/siat4080 src/20241025/train.py -lr 5e-5 -ct no_control --feature_type shcoeff_order2 --guidance_scale 1.0 --batch_size 4
 
-NODE=v28 GPU=3 NAME=face5e-5 bin/siat4080 src/20241025/train.py -lr 5e-3 -ct no_control --feature_type shcoeff_order2 --guidance_scale 1.0 --batch_size 4
+#NODE=v28 GPU=3 NAME=face5e-5 bin/siat4080 src/20241025/train.py -lr 5e-3 -ct no_control --feature_type shcoeff_order2 --guidance_scale 1.0 --batch_size 4
+
+#v3 continue from v0 1e-4
+NODE=v23 GPU=0 NAME=face1e-4 bin/silocal src/20241025/train.py -lr 1e-4 -ct no_control --feature_type shcoeff_order2 --guidance_scale 1.0 --batch_size 16 -ckpt output/20241025/multi_mlp_fit/lightning_logs/version_0/checkpoints/epoch=000019.ckpt
