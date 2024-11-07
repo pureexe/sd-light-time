c443be6d1c287231d11c2ad5441501b34f59cf95
pakkapon phongthawee <pure.gif@gmail.com>
2024-11-07 18:15:25 +0700

    RF inversion have unexplainble problem right now

--- src/20241104/sd35_medium.py
+++ src/20241104/sd35_medium.py
@@ -2,13 +2,14 @@ import torch
 from diffusers import StableDiffusion3Pipeline
 from attention_processor import RelightAttnProcessor2_0
 
-pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3.5-medium", torch_dtype=torch.bfloat16)
+pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3.5-medium", torch_dtype=torch.bfloat16, safety_checker=None)
 pipe = pipe.to("cuda")
-pipe.transformer.set_attn_processor(RelightAttnProcessor2_0())
+#pipe.transformer.set_attn_processor(RelightAttnProcessor2_0())
 
 image = pipe(
-    "A capybara holding a sign that reads Hello World",
+    #"A capybara holding a sign that reads Hello World",
+    "A photo of Lumine from Genshin impact",
     num_inference_steps=40,
-    guidance_scale=4.5,
+    guidance_scale=4.5
 ).images[0]
-image.save("src/20241104/capybara2.png")
+image.save("src/20241104/lumine.png")
